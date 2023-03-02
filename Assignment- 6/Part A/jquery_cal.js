7ea022593ec199ee93e9becf5a6acf570cdac184
Atharvaa97 <danke.a@northeastern.edu>
2023-03-01 21:24:48 -0500

    Added UI and functionality for calculator

--- Assignment- 6/Part A/jquery_cal.js
+++ Assignment- 6/Part A/jquery_cal.js
@@ -1,4 +1,58 @@
 $(document).ready(function () {
   var storedUsername = window.localStorage.getItem("username");
-  $(".username_title").text("Welcome, " + storedUsername + "!");
+  $(".navbutton_title").text(storedUsername);
+
+  $("#firstNumber").keyup((e) => {
+    if ($.isNumeric($("#firstNumber").val()) == false) {
+      alert("Please enter a number");
+      $("#firstNumber").val("");
+    }
+  });
+  $("#secondNumber").keyup((e) => {
+    if ($.isNumeric($("#secondNumber").val()) == false) {
+      alert("Please enter a number");
+      $("#secondNumber").val("");
+    }
+  });
+  $("#add").click((e) => {
+    if ($("#firstNumber").val() == "" || $("#secondNumber").val() == "") {
+      alert("Please enter both numbers");
+      return;
+    }
+    let num1 = parseInt($("#firstNumber").val());
+    let num2 = parseInt($("#secondNumber").val());
+    let result = num1 + num2;
+    $("#result").val(result);
+  });
+  $("#sub").click((e) => {
+    if ($("#firstNumber").val() == "" || $("#secondNumber").val() == "") {
+      alert("Please enter both numbers");
+      return;
+    }
+    let num1 = parseInt($("#firstNumber").val());
+    let num2 = parseInt($("#secondNumber").val());
+    let result = num1 - num2;
+    $("#result").val(result);
+  });
+  $("#mult").click((e) => {
+    if ($("#firstNumber").val() == "" || $("#secondNumber").val() == "") {
+      alert("Please enter both numbers");
+      return;
+    }
+    let num1 = parseInt($("#firstNumber").val());
+    let num2 = parseInt($("#secondNumber").val());
+    let result = num1 * num2;
+    $("#result").val(result);
+  });
+  $("#divi").click((e) => {
+    if ($("#firstNumber").val() == "" || $("#firstNumber").val() == "") {
+      alert("Please enter both numbers");
+      return;
+    }
+    let num1 = parseInt($("#firstNumber").val());
+    let num2 = parseInt($("#secondNumber").val());
+    let result = num1 / num2;
+    $("#result").val(result);
+  });
 });
+
