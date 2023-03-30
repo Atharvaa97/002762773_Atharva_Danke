fa6adf8145e8e9086e73d96dc0f73a35031f4c1a
Atharvaa97 <danke.a@northeastern.edu>
2023-03-30 07:32:41 -0400

    Added map function

--- Assignment-9/Frontend/src/pages/About/About.jsx
+++ Assignment-9/Frontend/src/pages/About/About.jsx
@@ -9,24 +9,24 @@ const Home = () => {
       <Navbar />
       <img src={AboutImage} alt="NUFlix logo" />
       <Card
-        header={"Don't just watch NUFlix, work for NUFlix!"}
+        header={"About Us?"}
         cardContent={
-          "Join our team and become a NUFlix superstar! P.S. Binge-watching is part of few job description!"
+          "We’re more than just a company, we’re a binge-worthy experience."
         }
       />
       
       <div className="text-content">
+        <h2 class="Heading_text">What is NUFlix?</h2>
+        <br></br>
+        <p>
+        NUFlix is a streaming service that brings you endless hours of entertainment at your fingertips. From the latest blockbusters to your favorite TV shows, we've got it all. With a vast library of movies and TV series, you'll never run out of things to watch. Whether you're looking for action, romance, comedy, or drama, NUFlix has something for everyone.
+        </p>
+        <br></br>
+        <br></br>
         <h2 class="Heading_text">Why choose NUFlix Careers?</h2>
         <br></br>
         <p>
         NUFlix is a well-known and respected brand in the entertainment industry, and working for NUFlix can provide excellent opportunities for career growth and development. We offer a dynamic and fast-paced work environment, where employees can work on cutting-edge projects and collaborate with some of the brightest minds in the industry.
-        </p>
-        <br></br>
-        <br></br>
-        <h2 class="Heading_text">Find your dream job today....</h2>
-        <br></br>
-        <p>
-          NUFlix Careers provides students and alumni with access to the best job listings in a variety of industries. Our platform features hundreds of job postings from top employers in the Boston area and beyond, with opportunities ranging from entry-level positions to senior management roles. With our easy-to-use search filters, you can find the right job that matches your qualifications and interests. Additionally, our platform offers a range of career resources, including resume-building tools, interview preparation guides, and networking events, to help you succeed in your job search and career development.
         </p>
       </div>
     </div>
--- Assignment-9/Frontend/src/pages/Contact/Contact.jsx
+++ Assignment-9/Frontend/src/pages/Contact/Contact.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Card from "../../componets/Card";
 import Navbar from "../../componets/Navbar";
+import ContactImage from "../../images/Contact.jpg";
 
 const Contact = () => {
   return (
-    <div className="container">
+    <div className="contact-container">
       <Navbar />
+      <img src={ContactImage} alt="NUFlix logo" />
       <Card
-        header={"Contact OGS"}
+        header={"Contact Us!"}
         cardContent="
-        For any information 
+        Our phones are on and our inbox is open. 
         Contact us at:
-        857-364-1519
+        857-693-8526
         "
       />
+      <form>
+  <label htmlFor="name">Name:</label>
+  <input type="text" id="name" name="name" required />
+
+  <label htmlFor="email">Email:</label>
+  <input type="email" id="email" name="email" required />
+
+  <label htmlFor="message">Message:</label>
+  <textarea id="message" name="message" rows="5" required></textarea>
+<br></br>
+  <button type="submit">Send</button>
+</form>
+
     </div>
   );
 };
